eb2141d7481edb9f720b43a9060935faed82494c
alecsch <98351016+alecsch@users.noreply.github.com>
2022-05-02 17:36:12 -0500

    Add files via upload
    Reformatted, updated for better adaptability

--- Python src/GUI.py
+++ Python src/GUI.py
@@ -18,7 +18,7 @@ def draw_figure(canvas, figure):
     return figure_canvas_agg
 
 layout = [
-    [sg.Text("Plot test")],
+    [sg.Text("Select a port:")],
     [sg.Listbox(testlist,
                 enable_events=True,
                 size=(None, len(testlist)),
@@ -26,6 +26,7 @@ layout = [
                 key = 'port'),
      ],
     [sg.Radio('Collect','opt', key = 'Collect'), sg.Radio('Plot','opt', key = 'Plot'), sg.Radio('Collect and Plot','opt', key = 'Collect and Plot'), sg.Radio('FFT','opt', key = 'FFT')],
+    [sg.Text("", key = 'Error')],
     [sg.Text('Please enter the data duration (seconds)', justification = 'right'), sg.InputText(key = 'time', justification = 'left')],
     [sg.Text('Please enter the Filename', justification='right'), sg.InputText(key = 'filename', justification = 'left')],
     [sg.Canvas(key="-CANVAS-")],
@@ -54,36 +55,72 @@ while True:
     
     if event in (sg.WINDOW_CLOSED, "Quit"):
         break
-        
-    
-    if event == 'port':
-        print(window['port'].get())
-        #Insert port connection here
-        
-        
-    if event == 'Collect':
-        #Save to CSV file function call
-        print("collect")
-        
-    if event == 'Plot':
-        print("plot")
-        
-    if event == 'Collect and Plot':
-        print("collect and plot")
-        
-    if event == 'time':
-        print('time')
-        
-    if event == 'filename':
-        print('filename')
-        
-    if event == 'FFT':
-        print("plot fft")  
-        
+
     if event == 'Run':
-        print(window['port'].get())
-        print(window['time'].get())
-        print(window['filename'].get())
+        if window['port'].get():
+            window['Error'].update(value = ' ')
+            if window['port'].get()[0] == 1 or window['port'].get()[0] == "":
+                window['Error'].update(value = 'Invalid port, please Retry')
+            else:
+                window['Error'].update(value = ' ')
+                print('connect')
+
+                if not window['time'].get():
+                    window['Error'].update(value = 'Enter a Time Desired')
+                else:
+                    #Define time var
+                    window['Error'].update(value = ' ')
+                    if window['Collect'].get():
+                        print("Collect")
+                        #Collection code
+                        if window['filename'].get() == '':
+                            window['Error'].update(value = 'Please enter a Valid file name')
+                        else:
+                            print(window['filename'].get())
+                            window['Error'].update(value = 'Collecting Data...')
+                            #delay(time)
+                            window['Error'].update(value = 'Collection Complete!')
+                            
+                    elif window['Plot'].get():
+                        print("Plot")
+                        #Plotting code
+                        #Does not require filename
+                        window['Error'].update(value = 'Collecting Data...')
+                        # Display realtime plotting
+                        window['Error'].update(value = 'Collection Complete!')
+                        
+                    elif window['Collect and Plot'].get():
+                        print("Collect and Plot")
+                        #Collect and Plotting Code
+                        if window['filename'].get() == '':
+                            window['Error'].update(value = 'Please enter a Valid file name')
+                        else:
+                            print(window['filename'].get())
+                            window['Error'].update(value = 'Collecting Data...')
+                            # Display realtime plotting
+                            window['Error'].update(value = 'Collection Complete!')
+
+                        
+                    elif window['FFT'].get():
+                        print("FFT")
+                        if window['filename'].get() == '':
+                            window['Error'].update(value = 'Please enter a Valid file name')
+                        else:
+                            print(window['filename'].get())
+                            window['Error'].update(value = 'Collecting Data...')
+                            #Delay(Time)
+                            window['Error'].update(value = 'Collection Complete!')
+                            #Display FFT Plot
+                    else:
+                        window['Error'].update(value = 'Select Desired Data output')
+                
+        else:
+            window['Error'].update(value = 'Select a port')
+            event == ''
+            
+
+            #FFT and plotting code
+            
         
     
         #INS plot function call
