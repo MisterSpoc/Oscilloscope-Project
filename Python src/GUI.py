5c2b995f4455f16549101220adf4b375dccb680e
alecsch <98351016+alecsch@users.noreply.github.com>
2022-05-02 14:50:32 -0500

    Add files via upload

--- Python src/GUI.py
+++ Python src/GUI.py
@@ -9,7 +9,7 @@ matplotlib.use("Agg")
 
 fig = matplotlib.figure.Figure(figsize=(5, 4), dpi=100)
 t = np.arange(0, 3, .01)
-
+testlist = [1,2,3,4,5]
 
 def draw_figure(canvas, figure):
     figure_canvas_agg = FigureCanvasTkAgg(figure, canvas)
@@ -19,16 +19,26 @@ def draw_figure(canvas, figure):
 
 layout = [
     [sg.Text("Plot test")],
+    [sg.Listbox(testlist,
+                enable_events=True,
+                size=(None, len(testlist)),
+                auto_size_text=True,
+                key = 'port'),
+     ],
+    [sg.Radio('Collect','opt', key = 'Collect'), sg.Radio('Plot','opt', key = 'Plot'), sg.Radio('Collect and Plot','opt', key = 'Collect and Plot'), sg.Radio('FFT','opt', key = 'FFT')],
+    [sg.Text('Please enter the data duration (seconds)', justification = 'right'), sg.InputText(key = 'time', justification = 'left')],
+    [sg.Text('Please enter the Filename', justification='right'), sg.InputText(key = 'filename', justification = 'left')],
     [sg.Canvas(key="-CANVAS-")],
-    [sg.Button('Standard Plot'), sg.Button('Plot FFT')],
-    [sg.Button('Exit')]
+    [sg.Button('Run'), sg.Button('Exit')]
+    
+    
     
     # Buttons can be added to this 
     # list as wished, they become events
 ]
 
 window = sg.Window(
-    "Matplotlib Single Graph",
+    "Oscilloscope GUI",
     layout,
     location=(0, 0),
     finalize=True,
@@ -41,16 +51,42 @@ while True:
     event, values = window.read()
     if event == 'Exit':
         break
+    
+    if event in (sg.WINDOW_CLOSED, "Quit"):
+        break
         
-    if event == 'Standard Plot':
-        fig.add_subplot(111).plot(t, 2 * np.sin(2 * np.pi * t))
-        draw_figure(window["-CANVAS-"].TKCanvas, fig)
+    
+    if event == 'port':
+        print(window['port'].get())
+        #Insert port connection here
         
-        #Just example function, best practice to call function for plot of signal here
         
-    elif event == 'Plot FFT':
-        fig.add_subplot(111).plot(t, np.sin(2 * np.pi * t))
-        draw_figure(window["-CANVAS-"].TKCanvas, fig)
+    if event == 'Collect':
+        #Save to CSV file function call
+        print("collect")
+        
+    if event == 'Plot':
+        print("plot")
+        
+    if event == 'Collect and Plot':
+        print("collect and plot")
+        
+    if event == 'time':
+        print('time')
+        
+    if event == 'filename':
+        print('filename')
+        
+    if event == 'FFT':
+        print("plot fft")  
+        
+    if event == 'Run':
+        print(window['port'].get())
+        print(window['time'].get())
+        print(window['filename'].get())
+        
+    
+        #INS plot function call
         
         #Example currently, call plot fft when ready for implementation
         
